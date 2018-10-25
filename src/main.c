01f077b970d0e383e2f2aecd0b11d8796933dc3f
Tyler <tyler.evans116@gmail.com>
2018-10-24 22:15:33 -0600

    simple argument handling

--- src/main.c
+++ src/main.c
@@ -7,24 +7,51 @@
 
 int main(int argc, char **argv)
 {
+	char command;
+	const struct option longOptions[] = 
+	{
+		{"help", no_argument, 0, 'h'},
+		{"version", no_argument, 0, 'v'}
+	};
 
-	struct Guid *guid = (struct Guid *)calloc(1, sizeof(struct Guid));
-	newGuid(guid);
-	printf("guid: %s \n", guid->value);
-	free(guid->value);
-	free(guid);
+	while (1) {
+		int optionIndex = -1;
+		int c= getopt_long(argc, argv, "hv", longOptions, &optionIndex);
 
-	char *test = calloc(12, sizeof(char));
-	strcpy(test, "Hello World");
-	struct Todo *todo = calloc(1, sizeof(struct Todo));;
-	createTodo(todo, test);
+		if (c== -1)
+		{
+			break;
+		}
 
-	printf("id: %s name: %s \n", todo->id->value, todo->name);
+		switch (c)
+		{
+			case 0:
+				// long option hit
+				command = longOptions[optionIndex].val;
+				break;
+			case 'h':
+				command = 'h';
+				break;
+			case 'v':
+				command = 'v';
+				break;
+		}
 
-	free(todo->id->value);
-	free(todo->id);
-	free(todo->name);
-	free(todo);
+		if (command)
+		{
+			break;
+		}
+	}
+
+	switch (command)
+	{
+		case 'h':
+			printf("help text \n");
+			break;
+		case 'v':
+			printf("version text \n");
+			break;
+	}
 
 	return 0;
 }
